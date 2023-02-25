1d023d0fb1632042500cd994288253b44fa3ff64
Jimmy <jimmyrss1102@gmail.com>
2023-02-26 01:41:50 +0800

    feat! [RsModal] : 添加`RsModal`组件

--- libs/index.tsx
+++ libs/index.tsx
@@ -1,1 +1,2 @@
-export { default as RsTabs } from './RsTabs/index';
+export { default as RsTabs } from './components/RsTabs/index';
+export { default as RsModal } from './components/RsModal/index';
--- libs/types/components.d.ts
+++ libs/types/components.d.ts
@@ -1,3 +1,5 @@
-import RsTabs from 'libs/RsTabs/index';
+import RsTabs from 'libs/components/RsTabs/index';
+import RsModal from 'libs/components/RsModal/index';
 
 export declare const RsTabs: typeof RsTabs;
+export declare const RsModal: typeof RsModal;
